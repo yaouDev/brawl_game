3c29c699ef6050fc11d8d3a3422c624389520561
yaouOps <augustjansson@outlook.com>
2021-10-31 18:41:37 +0100

    input cont

--- brawl_game/brawl_game/Assets/Scripts/PlayerMovement.cs
+++ brawl_game/brawl_game/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private InputMaster controls;
     [SerializeField] private float movementSpeed;
+
+    private void Awake()
+    {
+        controls = new InputMaster();
+        controls.Player.Test.performed += ctx => Test();
+    }
 
     private void Update()
     {
         
+    }
+
+    void Test()
+    {
+        Debug.Log("test");
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
     }
 }
